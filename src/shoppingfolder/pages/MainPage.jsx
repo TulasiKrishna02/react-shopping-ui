861a3f284c36521f186aee7c231d39425dcd899d
TulasiKrishna02 <tulasikrishnadokku@gmail.com>
2024-06-13 23:26:33 +0530

    shopping react ui

---
+++ src/shoppingfolder/pages/MainPage.jsx
@@ -0,0 +1,28 @@
+
+
+import React,{useState}from 'react'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+import Collections from '../components/Collections'
+import Footer from '../components/Footer'
+
+import { Gents,Ladies } from '../data';
+import WomanCollection from '../components/WomanCollection'
+
+const MainPage = () => {
+  const [gentsFashon,setGentsFashon] = useState(Gents)
+  const [ladiesFashon,setLadiesFashon] = useState(Ladies)
+  console.log(Gents);
+  return (
+    <div>
+      <Header/>
+      <Banner/>
+      <Collections gentsFashon={gentsFashon}/>
+      <WomanCollection ladiesFashon={ladiesFashon} />
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default MainPage
